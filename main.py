7a2c0fcc53247ee8803aa364d62b15d1471ab1c7
İlhan Akbudak <74368391+ilhanakbudak@users.noreply.github.com>
2021-10-01 06:27:49 +0000

    I'm done!

---
+++ main.py
@@ -0,0 +1,15 @@
+import numpy
+
+r1 = int(input("Enter number 1: "))
+r2 = int(input("Enter number 2: "))
+
+print(r1**r2)
+
+logr1 = numpy.log(r1)
+student_id = "0076509"
+print(logr1)
+
+print("Mission Completed!")
+
+print("My student id:", student_id)
+
